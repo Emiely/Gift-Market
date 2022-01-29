eb28cf02d449fe4d7d907a4e41cc7b931611a858
Allsknder <noorshams012@gmail.com>
2022-01-29 15:20:28 +0200

    Added the project files

---
+++ footer.php
@@ -0,0 +1,66 @@
+
+<!DOCTYPE html>
+<html dir="rtl" lang="ar" >
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>complete responsive flower website design tutorial</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600&display=swap" rel="stylesheet">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+
+<!-- footer section starts  -->
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>روابط سريعة</h3>
+            <a href="index.php">الرئيسية</a>
+            <a href="about.php">من نحن</a>
+            <a href="prodect.php">منتجاتنا</a>
+            <a href="more.php">المناسبات</a>
+            <a href="call us.php">اتصل بنا</a>
+        </div>
+
+        <div class="box">
+            <h3>روابط اضافية</h3>
+            <a href="https://www.facebook.com/%D8%AD%D8%A8-105984294555332/">حسابي</a>
+            <a href="#">طلبي</a>
+            <a href="#">مفضلات</a>
+        </div>
+
+        <div class="box">
+            <h3>الموقع</h3>
+            <a href="#">العراق</a>
+            <a href="#">بغداد</a>
+            <a href="#">الصليخ</a>
+           
+        </div>
+
+        <div class="box">
+            <h3>معلومات الاتصال</h3>
+            <a href="#"> 009647707841080 </a>
+            <a href="#">Email</a>
+            <a href="https://www.facebook.com/Laserbeauty990/">facebook</a>
+            
+        </div>
+
+    </div>
+
+    <div class="credit"> created by <span> Eng. Tasneem Almohammed </span> | كل الحقوق محفوظة </div>
+
+</section>
+
+<!-- footer section ends -->
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html dir="rtl" lang="ar" >
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>complete responsive flower website design tutorial</title>
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@600&display=swap" rel="stylesheet">
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia&effect=fire">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
+    <link rel="stylesheet" href="css/style-rtl.css">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+
+<!-- header section starts  -->
+
+<header>
+
+    <input type="checkbox" name="" id="toggler">
+    <label for="toggler" class="fas fa-bars"></label>
+
+    <a href="#" class="logo">LAser<span>Beauty</span></a>
+
+    <nav class="navbar">
+        <a href="index.php">الرئيسية</a>
+        <a href="about.php">من نحن</a>
+        <a href="prodect.php">منتجاتنا</a>
+        <a href="more.php">المناسبات</a>
+        <!-- <a href="#review">المراجعة</a> -->
+        <a href="call us.php">اتصل بنا</a>
+    </nav>
+
+    <div class="icons">
+        <a href="https://iwtsp.com/9647707841080" class="fas fa-phone ">  : 009647707841080  </a>
+      
+    </div>
+
+</header>
+
+<!-- header section ends -->
+
+
